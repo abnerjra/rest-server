2821c141b29ad889c37f9c23eb6c8288c6e2a045
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-20 13:52:22 -0600

    feat: prefix api added

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,9 +3,10 @@ import { TodoRoutes } from "./todos/routes";
 
 export class AppRoutes {
     static get routes(): Router {
+        const prefix = '/api';
         const router = Router();
 
-        router.use('/', TodoRoutes.routes);
+        router.use(prefix, TodoRoutes.routes);
 
         return router;
 
