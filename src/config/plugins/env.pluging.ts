1a15ac1d7ff4214e0829e7113c6b016b418d44ad
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-20 16:49:51 -0600

    feat: mapping new environment variables

--- src/config/plugins/env.pluging.ts
+++ src/config/plugins/env.pluging.ts
@@ -4,4 +4,9 @@ import * as env from 'env-var';
 export const envConfig = {
     PORT: env.get('PORT').default(3000).asPortNumber(),
     PUBLIC_PATH: env.get('PUBLIC_PATH').default('public').asString(),
+    POSTGRES_URL: env.get('POSTGRES_URL').asString(),
+    POSTGRES_USER: env.get('POSTGRES_USER').asString(),
+    POSTGRES_DB: env.get('POSTGRES_DB').asString(),
+    POSTGRES_PORT: env.get('POSTGRES_PORT').asPortNumber(),
+    POSTGRES_PASSWORD: env.get('POSTGRES_PASSWORD').asString(),
 }
